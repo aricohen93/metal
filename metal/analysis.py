1c37f65ad21ad5d00c19e66cedb9a894fb38e1bb
Ariel Cohen <ariel.cohen2@aphp.fr>
2026-01-13 13:03:41 +0100

    fix ?

--- metal/analysis.py
+++ metal/analysis.py
@@ -216,7 +216,10 @@ def lf_summary(L, Y=None, lf_names=None, est_accs=None, **kwargs):
             f1 = f1_score(Y, L_dense[:, i], **kwargs)
             precision = precision_score(Y, L_dense[:, i], **kwargs)
             support_precision = np.sum(
-                Y[L_dense[:, i] == kwargs.get("pos_label", 0)]
+                Y[
+                    (L_dense[:, i] != kwargs.get("abstention_value", 0))
+                    & (Y == kwargs.get("pos_label", 1))
+                ]
             ).item()
             support_precisions.append(support_precision)
             recall = recall_score(Y, L_dense[:, i], **kwargs)
